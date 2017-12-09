f907280f63884a750100fdbfafa63b897de5175e
littlepinkbeast <34397751+littlepinkbeast@users.noreply.github.com>
2017-12-09 09:11:33 -0600

    Add files via upload

--- dtdice.rb
+++ dtdice.rb
@@ -7,8 +7,9 @@
 # discarded dice
 # q - Quiet mode. Only report the total, not the individual die rolls.
 # t## - Target. Keep explosions only if necessary to reach ##, or if there is no other option.
-# x - Extra explosions. Dice explode on nines as well as tens.
 # v - Verbose. Print debugging information.
+# x - Extra explosions. Dice explode on nines as well as tens.
+
 
 avoid_explosions = false
 quiet_mode = false
@@ -26,6 +27,33 @@ kept = k.to_i
 if (rolled == 0 || kept == 0) then
 	puts "Invalid or zero entry for number of dice. Results will be incorrect."
 end
+
+# D:tD dice rules: The most you can roll is 10k10. Excess rolled dice convert into kept dice at 2 to 1.
+# If you end up at less than ten kept dice and are at 11 rolled dice, the extra one is just lost.
+# If kept is 10 or greater, any excess rolled or kept dice become 5 points of flat bonus.
+
+while (rolled > 11 && kept < 10) do
+	rolled -= 2
+	kept += 1
+end
+
+if kept >= 10 then
+	k_bonus = kept - 10
+	kept = 10
+	if rolled >= 10 then
+		r_bonus = rolled - 10
+		rolled = 10
+	end
+	flat_bonus += (k_bonus + r_bonus) * 5
+end
+
+# By the time we get here, we should have taken care of all situations in which rolled dice can be converted into some
+# kind of other bonus to the roll, and so if there's still an extra rolled die it just vanishes.
+
+if rolled > 10 then rolled = 10 end
+
+# The core die-roller function. "nines" is boolean, and specifies whether nines explode.
+# FOr each die, returns the value of the die and whether or not that die exploded.
 
 def explode(nines)
 	bang = false
@@ -73,11 +101,16 @@ prelim = Array.new
 rolled.times do
 	res, popped = explode(nines_explode)
 	kept_explosion = (kept_explosion || popped)
-	prelim.push res
+	prelim.push [res, popped]
 end
 prelim.sort!.reverse!
-puts prelim.to_s unless quiet_mode
+if (quiet_mode == false) then
+	prelim.each do |i|
+		print i[0].to_s + " "
+	end
+	print "\n"
+end
 result = 0
-0.upto(kept-1) { |x| result += prelim[x]}
+0.upto(kept-1) { |x| result += prelim[x][0]}
 result += flat_bonus
 puts result.to_s + ": " + kept_explosion.to_s
